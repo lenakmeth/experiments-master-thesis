2c4aabcca4ef6bf074517dd0811a2081674b7c75
lenakmeth <lenakmeth@gmail.com>
2020-11-17 14:26:38 +0100

    Revert "clean up"
    This reverts commit 9bf58c7b2d1463881f2a99bde9f8979d878849ea.

--- master_script.sh
+++ master_script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=serial_job_test    # Job name
-#SBATCH --mail-type=ALL        # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=eleni.metheniti@irit.fr     # Where to send mail	
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --mem=1gb                     # Job memory request
 #SBATCH --time=00:05:00               # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log   # Standard output and error log
-axn7833; osirim-slurm.irit.fr; 18/11/2019
+bGMTSRXJ; osirim-slurm.irit.fr; 17/11/2019
 
-module load python
 
-python /projets/melodi/lenakmeth/main.py --lang hun
+module load python3
+
+python3 /projets/melodi/lenakmeth/main.py --lang hun
